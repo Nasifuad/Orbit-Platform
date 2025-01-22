8c23f53cff5196c7b2f7d4e74e12d8ba73fc12ab
Nasif <nasifuad007@gmail.com>
2025-01-22 07:45:23 +0600

    Implementing backend

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 import cors from "cors";
-
 const app = express();
 app.use(cors());
 app.use(express.json());
+import connectDB from "./DB/mongoDB.js";
+import router from "./router/auth_route.js";
+connectDB() &&
+  app.listen(process.env.PORT, () => console.log("Server is running"));
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.use("/api", router);
